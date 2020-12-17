0aabe9af4bed6b72c23c2dea92ccf740929d9f53
jean <jeandesir84@gmail.com>
2020-12-18 00:18:29 +0100

    date of birth auteur

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,14 +7,18 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('auteur')
-            ->add('dateDeNaissance')
+            ->add('dateDeNaissance',BirthdayType::class, [
+    'placeholder' => [
+        'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
+    ]
+])
         ;
     }
 
